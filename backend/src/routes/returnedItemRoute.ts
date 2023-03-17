39b27992ea9564bd43a1711820b4fcba3e73140f
JB <jbevangelista.0107@gmail.com>
2023-03-17 15:02:36 +0800

    Create Returned Item

--- backend/src/routes/returnedItemRoute.ts
+++ backend/src/routes/returnedItemRoute.ts
@@ -1,10 +1,24 @@
 import { Router } from "express";
-import { getAllReturnedItemsController } from "../controllers/returnedItemController";
+import {
+  createReturnedItemController,
+  deleteReturnedItemController,
+  getAllReturnedItemsController,
+  getReturnedItemController,
+  updateReturnedItemController,
+} from "../controllers/returnedItemController";
 import middleware from "../middleware/middleware";
 
 const router: Router = Router();
 
 // With Token
 router.get("/getAllReturnedItems", middleware, getAllReturnedItemsController);
+router.post("/createReturnedItem", middleware, createReturnedItemController);
+router.get("/getReturnedItem/:id", middleware, getReturnedItemController);
+router.put("/updateReturnedItem/:id", middleware, updateReturnedItemController);
+router.post(
+  "/deleteReturnedItem/:id",
+  middleware,
+  deleteReturnedItemController
+);
 
 export default router;
